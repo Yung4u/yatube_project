42dbf07aec7ccce0e0ed21ee71ba62a0b9733cce
Орлов Сергей <jokeblablo@gmail.com>
2023-01-11 16:46:15 +0300

    3/13 5/7 6/6 до настройки шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Социальная сеть для блогеров',
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request):
-    template = 'posts/group_posts.html'
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Социальная сеть для блогеров',
-        'text': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
