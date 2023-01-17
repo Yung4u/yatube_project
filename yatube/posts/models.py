4cd7fe5e2fdf8009a44ccca7277d8652e3966e06
Орлов Сергей <jokeblablo@gmail.com>
2023-01-17 16:10:16 +0300

    is working

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,17 +6,18 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="название группы")
+    slug = models.SlugField(unique=True, verbose_name="ссылка группы")
+    description = models.TextField(verbose_name="текст группы")
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name="текст поста")
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name="дата поста")
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -29,3 +30,9 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         related_name='post'
     )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('-pub_date',)
